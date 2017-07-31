fdf5041c9cc309b652d11a52ae6d0321201f66af
AlexLeMes <alexlem17@gmail.com>
2017-07-31 18:24:20 +0400

    backup_week10

---
+++ GAM111_assignment02/TowerDefenseGame/Assets/scripts/followWaypoints.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followWaypoints : MonoBehaviour {
+
+    public waypointSystem myPath;
+
+    public float speed = 1f;
+
+    int currentPositionIndex;
+
+
+	void Start () {
+        currentPositionIndex = 0;
+	}
+
+	void Update ()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, myPath.positions[currentPositionIndex], speed * Time.deltaTime);
+
+        if(transform.position == myPath.positions[currentPositionIndex])
+        {
+            currentPositionIndex++;
+            if(currentPositionIndex == myPath.positions.Length)
+            {
+                currentPositionIndex = 0;
+            }
+        }
+	}
+}
---
+++ GAM111_assignment02/TowerDefenseGame/Assets/scripts/spawnTower.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class spawnTower : MonoBehaviour {
+
+    GameObject player;
+
+    public GameObject tower01;
+    public GameObject tower02;
+
+    GameObject spawnedTower;
+
+    public bool canSpawnHere = true;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        canSpawnHere = true;
+    }
+
+    public void spawnTowerOne()
+    {
+        if(canSpawnHere == true)
+        {
+            spawnedTower = Instantiate(tower01);
+            spawnedTower.transform.position = player.transform.position;
+        }
+        else
+        {
+            Debug.Log("cannotSpawnTowerHere");
+        }
+    }
+
+}
